b4bedec6335ae399d98d8322f451d84e62b42316
LukeK2021 <89875022+LukeK2021@users.noreply.github.com>
2023-10-04 14:49:11 +0000

    Springboot server setup

---
+++ server/LKserver/src/main/java/com/example/LKserver/LKserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.LKserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LKserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LKserverApplication.class, args);
+	}
+
+}
