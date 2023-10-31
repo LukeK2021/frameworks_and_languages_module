b346b5cd1ece8795c0f4bb71c2c968d66f13ec3c
LukeK2021 <89875022+LukeK2021@users.noreply.github.com>
2023-10-31 14:07:47 +0000

    2 fail 14 pass 4 skipped 3 errors

--- server/server.py
+++ server/server.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime
+import uuid
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, origins="http://localhost",
+      allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+      supports_credentials=True)
 
 
 items = [ #sample data
-    {"id": 1 ,"user_id": "user1234", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
-    {"id": 2 ,"user_id": "user123", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
-    {"id": 3 ,"user_id": "user12", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()}
+    {"id": uuid.uuid4() ,"user_id": "user1234", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
+    {"id": uuid.uuid4() ,"user_id": "user123", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
+    {"id": uuid.uuid4() ,"user_id": "user12", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()}
 ]
 
 #get home page
@@ -28,24 +31,29 @@ def get_id(userId):
     for user in items:
         if user['id']==userId:
             return user, 200
-    return {'error':'not found'}, 404
+    return jsonify({'error':'not found'}), 404
 
 
 #post a new item
 @app.route('/item',methods=['POST'])
 def new_item():
-    post = {"id": len(items)+1, 'user_id':request.json['user_id'],'keywords':request.json['keywords'], 'description':request.json['description'], 'lat':request.json['lat'], 'lon':request.json['lon'], "date_time": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-    items.append(post)
-    return post, 201
+    inputData = request.get_json()
+    if "user_id" not in inputData:
+        return jsonify({"error": "Missing 'id' field"}), 405
+    else:
+        post = {"id": len(items)+1, 'user_id':request.json['user_id'],'keywords':request.json['keywords'], 'description':request.json['description'], 'lat':request.json['lat'], 'lon':request.json['lon'], "date_time": datetime.now().isoformat(),"date_from": datetime.now().isoformat()}
+        items.append(post)
+        return post, 201
+    
 
 #delete an item using id param
 @app.route('/item/<int:Id>',methods=['DELETE'])
-def remove():
+def remove(Id):
     for user in items:
         if user['id']==Id:
             items.remove(user)
-            return{"data":"deleted"}, 200
-        return {'error':'not found'}, 404
+            return jsonify({"data":"deleted"}), 204
+        return jsonify({'error':'not found'}), 404
 
 
 
