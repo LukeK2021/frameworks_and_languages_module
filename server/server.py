3698f47d596271b952367cfe2c79f4cd66e47c3d
LukeK2021 <89875022+LukeK2021@users.noreply.github.com>
2023-11-12 08:47:59 +0000

    updated comments and refactored methods get/delete

--- server/server.py
+++ server/server.py
@@ -10,12 +10,21 @@ CORS(app, origins="http://localhost:8000",
 
 
 items = [ #sample data
-    {"id": uuid.uuid4() ,"user_id": "user1234", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
-    {"id": uuid.uuid4() ,"user_id": "user123", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
-    {"id": uuid.uuid4() ,"user_id": "user12", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()}
+    {"id": 1 ,"user_id": "user1234", "keywords": ["hammer", "nails", "tools"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
+    {"id": 2 ,"user_id": "user123", "keywords": ["drill", "saw", "sander"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()},
+    {"id": 3 ,"user_id": "user12", "keywords": ["screwdriver", "tea", "welding iron"], "description": "A hammer and nails set. In canterbury", "lat": 51.2798438, "lon": 1.0830275,"date_from": datetime.now().isoformat()}
 ]
+base_url = 'http://localhost:8000'
 
-#get home page
+
+deleted_items = set() # set of deleted items
+
+
+@app.route('/items/', methods=['OPTIONS'])
+def options():
+    return '', 200
+
+#default page route
 @app.route('/',methods=['GET'])#specifying the url route and the http method to perform operations.
 def home():
     return "Home", 200
@@ -23,16 +32,23 @@ def home():
 #get all users
 @app.route('/items',methods=['GET'])
 def getusers():
-    return items, 200
+    return jsonify(items), 200
 
 #takes integer parameter to filter users data by their id as a path parameter https://flask.palletsprojects.com/en/3.0.x/api/#flask.Flask.route
-@app.route('/item/<int:userId>',methods=['GET'])
-def get_id(userId):
-    for user in items:
-        if user['id']==userId:
-            return user, 200
-    return jsonify({'error':'not found'}), 404
-
+#combining both methods as url syntax is identical according to brief.
+@app.route('/item/<int:Id>',methods=['GET','DELETE'])
+def get_id(Id):
+    if request.method == 'GET': #accessing the get method https://flask.palletsprojects.com/en/2.3.x/api/#flask.Request.method
+        for user in items:
+            if user['id'] == Id:
+                return jsonify(user), 200
+        return jsonify({'error': 'not found'}), 404
+    if request.method == 'DELETE':
+        for user in items:
+            if user['id']==Id:
+                items.remove(user)
+                return jsonify(user), 204
+        return jsonify({'error': 'not found'}), 404
 
 #post a new item
 @app.route('/item',methods=['POST'])
@@ -41,9 +57,9 @@ def new_item():
     if "user_id" not in inputData:
         return jsonify({"error": "Missing 'id' field"}), 405
     else:
-        post = {"id": len(items)+1, 'user_id':request.json['user_id'],'keywords':request.json['keywords'], 'description':request.json['description'], 'lat':request.json['lat'], 'lon':request.json['lon'], "date_time": datetime.now().isoformat(),"date_from": datetime.now().isoformat()}
+        post = {"id": len(items)+1 , 'user_id':request.json['user_id'],'keywords':request.json['keywords'], 'description':request.json['description'], 'lat':request.json['lat'], 'lon':request.json['lon'], "date_time": datetime.now().isoformat(),"date_from": datetime.now().isoformat()}
         items.append(post)
-        return post, 201
+        return jsonify(post), 201
     
 
 #delete an item using id param
@@ -52,10 +68,15 @@ def remove(Id):
     for user in items:
         if user['id']==Id:
             items.remove(user)
-            return jsonify({"data":"deleted"}), 204
+            return jsonify(user), 200
         return jsonify({'error':'not found'}), 404
 
-
+@app.route('/item/<string:keyword>',methods=['GET'])
+def get_items_by_keyword(keyword):
+    if not keyword:
+        return jsonify({'error': 'Keyword parameter is required'}), 400
+    filtered_items = [item for item in items if keyword.lower() in [kw.lower() for kw in item['keywords']]]
+    return jsonify(filtered_items)
 
 
 #entrypoint for flask application
